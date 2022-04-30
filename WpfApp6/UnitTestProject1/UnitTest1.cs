711f26bea945f23b07c97e05118a0794540c401a
169 <77526172+GlazryinDanila@users.noreply.github.com>
2022-04-30 11:39:57 +0400

    Экзамен

---
+++ WpfApp6/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1;
+            int y = 2;  
+            int z = 3;
+            WpfApp6 c = new WpfApp6();
+            int actual = c.Sum(x, y);
+            Assert.AreEqual(x, actual);
+        }
+    }
+}
---
+++ WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<TextBlock> matrx;
+        static Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            matrx = new List<TextBlock>();
+            FillMatrix();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (TextBlock a in matrx)
+                {
+                    a.Text = random.Next(0, 10).ToString();
+                }
+            }
+
+
+
+        }
+
+        private void FillMatrix()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    TextBlock temp = (TextBlock)GetChildren(MatrixGrid, i, j);
+                    matrx.Add(temp);
+                }
+            }
+        }
+
+        private static UIElement GetChildren(Grid grid, int row, int column)
+        {
+            foreach (UIElement child in grid.Children)
+            {
+                if (Grid.GetRow(child) == row
+                      &&
+                   Grid.GetColumn(child) == column)
+                {
+                    return child;
+                }
+            }
+            return null;
+        }
+    }
+}
